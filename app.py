5a9ce86cdbc02d713b900d07c604ec89aa459c5b
Muhammad Fakhrul Amin <80448871+mfakhrulam@users.noreply.github.com>
2023-06-13 23:12:30 +0700

    Clean up unnecessary code

--- app.py
+++ app.py
@@ -1,22 +1,16 @@
 import os 
-import tensorflow as tf
-from PIL import Image
-import numpy as np
-from flask import Flask, request, jsonify, make_response, send_file
-import requests
-import re
-import base64
 from io import BytesIO
-
-
-# from google.cloud import storage
+from PIL import Image
+from flask import Flask, request, jsonify, make_response
+import requests
+import tensorflow as tf
+import numpy as np
 
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 app = Flask(__name__)
 class_names = ['No DR', 'Mild', 'Moderate', 'Severe', 'Proliferative DR']
-
 
 # eye and no-eye model
 eye_classification_model = tf.keras.models.load_model('.\Eyes_classification\Eyes_Classification_Model.h5')
@@ -46,29 +40,6 @@ def predict_dr_image(input_arr):
     predicted_class = class_names[np.argmax(prediction)]
     return predicted_class
     
-    """def predict():
-    if 'image_url' in request.json:
-        image_url = request.json['image_url']
-        image = tf.keras.utils.load_img(response.content, target_size=(350,350))
-    
-    elif 'bucket_name' in request.json and 'image_path' in request.json:
-        bucket_name = request.json['bucket_name']
-        image_path = request.json['image_path']
-
-        storage_client = storage.Client()
-        bucket = storage_client.get_bucket(bucket_name)
-        blob = bucket.blob(image_path)
-        image = tf.keras.utils.load_img(blob.download_as_text(), target_size=(350,350))
-        
-    else:
-        return jsonify({'error': 'Invalid request'}), 400
-    
-    image = transform_image(image)
-    prediction = predict_image(eye_classification_model, image)
-    predicted_class = np.argmax(prediction)
-    
-    return jsonify({'class': predicted_class})"""
-    
 @app.route("/", methods=["GET"])
 def index():
     dict = {
@@ -91,13 +62,11 @@ def predict():
         # dr_img = transform_image(img, 350) # new model using 350x350
         eye_img = transform_image(img, 150)
         
-        # check the shape of the image
-        # return jsonify({'shape': list(eye_img.shape)})
-        # return jsonify({'shape': eye_img.tolist()})
-    
-        # make predictions to detect if image is a eye/retina
+        # make predictions to detect if the image has retina on it
         eye_prediction = predict_eye_image(eye_img)
         if eye_prediction:
+            # make predictions to detect severe levels of 
+            # diabetic retinopathy
             dr_prediction = predict_dr_image(dr_img)
             dict = {
                 'message': 'Diabetic Retinopathy Level',
@@ -113,7 +82,6 @@ def predict():
             'dr_class': dr_prediction
         }
         return jsonify(dict)
-           
     
 if __name__ == "__main__":
     app.run(debug = True)
